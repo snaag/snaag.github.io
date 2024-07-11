6c8de56ad8793ebd781c30a1d7d562723049cbcc
snaag <snaag.dev@gmail.com>
2024-07-12 03:22:50 +0900

    add components in HomePage - 프로필, 통계, 최근글들

--- src/pages/post-list/index.tsx
+++ src/pages/post-list/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import RecentPostList from "./RecentPostList";
+import Statistics from "./Statistics";
+import Profile from "./Profile";
 
 const PostList = () => {
     return (
         <div>
-            <h1>PostList</h1>
+            <Profile />
+            <Statistics />
+            <RecentPostList />
         </div>
     );
 };
