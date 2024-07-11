3b42141f9a72573ceffd350482e556ee839cbc17
snaag <snaag.dev@gmail.com>
2024-07-12 03:23:18 +0900

    add RecentPostList Component

---
+++ src/pages/post-list/RecentPostList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecentPostList = () => {
+    return (
+        <div>
+            <h2>RecentPostList</h2>
+        </div>
+    );
+};
+
+export default RecentPostList;
