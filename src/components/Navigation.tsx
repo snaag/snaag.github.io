1acced82281404d0c68bffbee52ce6eede4b175a
snaag <snaag.dev@gmail.com>
2024-07-12 03:22:01 +0900

    update style Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,10 +3,13 @@ import {Link} from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <div>
-            <Link to={'post'}>Post</Link>
-            <br/>
-            <Link to={'post-list'}>Post Detail</Link>
+        <div className="flex justify-between sticky top-0 bg-green-300">
+            <h2 className="hidden md:block">snaag 블로그</h2>
+            <div className="grid grid-cols-2">
+                <Link className="text-base" to={'/'}>메인으로</Link>
+                <Link className="text-base" to={'post-list'}>글 목록으로</Link>
+            </div>
+            <h2 className="block md:hidden">snaag</h2>
         </div>
     );
 };
