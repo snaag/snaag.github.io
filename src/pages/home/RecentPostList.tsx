62e709756d816ce3cca3311df1f9954ac853e937
snaag <snaag.dev@gmail.com>
2024-07-12 03:30:59 +0900

    update title style

--- src/pages/home/RecentPostList.tsx
+++ src/pages/home/RecentPostList.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RecentPostList = () => {
     return (
         <div>
-            <h2>RecentPostList</h2>
+            <h1 className="text-2xl text-center mb-3 font-bold md:text-left">최근 글 목록</h1>
         </div>
     );
 };
--- src/pages/home/Statistics.tsx
+++ src/pages/home/Statistics.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Statistics = () => {
     return (
         <div>
-            <h2>통계</h2>
+            <h1 className="text-2xl text-center mb-3 font-bold md:text-left">통계</h1>
         </div>
     );
 };
