91ed2662c28f8ad4b3ec1b5893711a5b03b70a67
snaag <snaag.dev@gmail.com>
2024-07-12 03:21:43 +0900

    update ErrorPage

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -3,10 +3,9 @@ import {Link} from "react-router-dom";
 
 const Error = () => {
     return (
-        <div>
-            Error!
-            <br/>
-            <Link to={'/'}>돌아가기</Link>
+        <div className="flex flex-col h-screen justify-center items-center">
+            <span className="text-xl mb-2">문제가 발생했어요!</span>
+            <Link className="text-base" to={'/'}>돌아가기</Link>
         </div>
     );
 };
