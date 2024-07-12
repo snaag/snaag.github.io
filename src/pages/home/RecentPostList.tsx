f39d7b4f5c594d39aaab5ff76954791076738a0b
snaag <snaag.dev@gmail.com>
2024-07-12 19:10:03 +0900

    update title margin - mb-3 -> m-4

--- src/pages/home/RecentPostList.tsx
+++ src/pages/home/RecentPostList.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RecentPostList = () => {
     return (
         <div>
-            <h1 className="text-2xl text-center mb-3 font-bold md:text-left">최근 글 목록</h1>
+            <h1 className="text-2xl text-center m-4 font-bold md:text-left">최근 글 목록</h1>
         </div>
     );
 };
--- src/pages/home/Statistics.tsx
+++ src/pages/home/Statistics.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Statistics = () => {
     return (
         <div>
-            <h1 className="text-2xl text-center mb-3 font-bold md:text-left">통계</h1>
+            <h1 className="text-2xl text-center m-4 font-bold md:text-left">통계</h1>
         </div>
     );
 };
