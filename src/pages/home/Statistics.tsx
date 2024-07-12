64256b541764dd721f12e1fe4135264cf8d1a132
snaag <snaag.dev@gmail.com>
2024-07-12 19:10:28 +0900

    api request test in Statistics component

--- src/pages/home/Statistics.tsx
+++ src/pages/home/Statistics.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import {getNamesAndCounts} from '../../apis/post';
 const Statistics = () => {
+    (async () => {
+        const ret: DtoNameAndCounts = await getNamesAndCounts();
+        console.log(ret);
+    })()
+
     return (
         <div>
             <h1 className="text-2xl text-center m-4 font-bold md:text-left">통계</h1>
