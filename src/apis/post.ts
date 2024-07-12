b00ae35d4c95d24bf7104a241cffd4f43f5951cf
snaag <snaag.dev@gmail.com>
2024-07-13 02:58:57 +0900

    modify api call code for authorization - use github authorization token

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -1,14 +1,21 @@
 import axios, {AxiosResponse} from 'axios';
 import data from '../assets/data.json';
 
-const BASE_URL = `https://api.github.com/repos/${data.repository.owner}/${data.repository.repo}`;
+const baseURL = `https://api.github.com/repos/${data.repository.owner}/${data.repository.repo}`;
+
+const authInstance = axios.create({
+    baseURL,
+    headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`
+    }
+})
 
 export const getLabels = async (): Promise<AxiosResponse<Label[]>> => {
-    return axios.get(BASE_URL+'/labels');
+    return authInstance.get( '/labels');
 }
 
 export const getLabelsForOneIssue = async (issueNo: number): Promise<AxiosResponse<Label[]>> => {
-    return axios.get(`${BASE_URL}/issues/${issueNo}/labels`);
+    return authInstance.get(`/issues/${issueNo}/labels`);
 }
 
 export const getIssuesByLabel = async (label: string): Promise<AxiosResponse<Issue[]>> => {
@@ -21,25 +28,33 @@ export const getIssuesByLabels = async (labels: string[]): Promise<AxiosResponse
         per_page: 100,
         labels: labels.map((label: string) => label.trim()).join(",")
     }
-    return axios.get(`${BASE_URL}/issues`, {params})
+    return authInstance.get('/issues', {params})
 }
 
 // 레이블 이름들과, 작성된 이슈들의 갯수를 반환
-export const getNamesAndCounts = async (): Promise<DtoNameAndCounts> => {
-    const ret: DtoNameAndCounts = {};
-    // TODO 정렬을 해서 보내주면 참 좋겠네
+export const getLabelCounts = async (): Promise<LabelCount[]> => {
+    const ret = [];
 
     const labels = await getLabels();
-    const labelNames = labels.data.map((label: Label) => label.name);
 
-    for(let i=0; i<labelNames.length; i++) {
-        const labelName = labelNames[i];
+    for (let i = 0; i < labels.data.length; i++) {
+        const labelName = labels.data[i].name;
+        const labelColor = labels.data[i].color;
         const issues = await getIssuesByLabel(labelName);
         const count = issues.data.length;
-        if(count > 0) {
-            ret[labelName] = count;
+        if (count > 0) {
+            ret.push({
+                name: labelName,
+                count,
+                color: labelColor
+            });
         }
     }
 
+    ret.sort((a: LabelCount, b: LabelCount) =>
+        a.count > b.count ? -1 :
+            a.count < b.count ? 1 :
+                0
+    )
     return ret;
 }
