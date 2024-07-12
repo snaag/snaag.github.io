1199c0511629c29a5e827aa92d001330641a304e
snaag <snaag.dev@gmail.com>
2024-07-13 04:40:56 +0900

    implementation PostList Page

--- src/pages/post-list/index.tsx
+++ src/pages/post-list/index.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link, useParams} from "react-router-dom";
+import {getIssuesByLabel, getLabelCounts} from "../../apis/post";
+import ListPostTitle from "../../components/ListPostTitle";
+import Category from "./Category";
 
 const PostList = () => {
+    const {label = ""} = useParams();
+    const [posts, setPosts] = useState<Issue[]>([]);
+
+    (async () => {
+        const response = await getIssuesByLabel(label);
+        setPosts(response.data);
+    })();
+
+
     return (
         <div>
-            <h2>글 목록 페이지</h2>
+            <h2 className="text-lg text-center font-bold truncate max-w-full m-4 p-2">{label}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4">
+                <div className="col-span-1 md:col-span-3">
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                    {
+                        posts.map((post, id) => <ListPostTitle post={post} key={id} />)
+                    }
+                </div>
+                <Category />
+            </div>
+
         </div>
     );
 };
