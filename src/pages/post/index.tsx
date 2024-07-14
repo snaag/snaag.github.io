15da9a15ee19f08d662c7d4b7212e3db6f311c24
snaag <snaag.dev@gmail.com>
2024-07-14 17:31:03 +0900

    load issue in Post Page

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
+import {getIssue} from "../../apis/post";
 
 const Post = () => {
-    const {postId} = useParams();
+    const {postId = ""} = useParams();
+    const [post, setPost] = useState<Issue>({} as Issue);
+
+    (async () => {
+        const response = await getIssue(postId);
+        setPost(response.data);
+    })()
 
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
-                Post {postId}
-            </h1>
+            <h1 className="text-3xl font-bold underline">{post.title}</h1>
+            <p>{post.body}</p>
         </div>
     );
 };
