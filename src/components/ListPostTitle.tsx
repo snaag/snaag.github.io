4825f87e94cd285212bf08884acb9a9ce85fb35c
snaag <snaag.dev@gmail.com>
2024-07-14 17:29:34 +0900

    add Link in ListPostTitle Component

--- src/components/ListPostTitle.tsx
+++ src/components/ListPostTitle.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Label from "./Label";
+import {Link} from "react-router-dom";
 
 interface Props {
     post: Issue
@@ -7,10 +8,13 @@ interface Props {
 const ListPostTitle = ({post}: Props) => {
     return (
         <div className="flex items-center mb-2">
+            <Link to={`/post/${post.number}`}>
+
             {
                 post.labels.map((label, id) => <Label label={label} key={id}/>)
             }
             <span className="ml-2 truncate w-full">{post.title}</span>
+            </Link>
         </div>
     );
 };
