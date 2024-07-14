c43f87886ca78de062f25a8f0032ccea88d5b56c
snaag <snaag.dev@gmail.com>
2024-07-14 17:29:57 +0900

    delete useless link and and add Link to title

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,12 +4,13 @@ import {Link} from "react-router-dom";
 const Navigation = () => {
     return (
         <div className="flex justify-between sticky top-0 bg-green-300 p-2 text-base md:p-3">
-            <h2 className="hidden font-bold md:block">snaag 블로그</h2>
-            <div className="grid grid-cols-2">
-                <Link to={'/'}>메인으로</Link>
-                <Link to={'post-list'}>글 목록으로</Link>
-            </div>
-            <h2 className="block font-bold md:hidden">snaag</h2>
+            <h2 className="hidden font-bold md:block">
+                <Link to={'/'}>snaag 블로그</Link>
+            </h2>
+            <Link to={'/'}>메인으로</Link>
+            <h2 className="block font-bold md:hidden">
+                <Link to={'/'}>snaag</Link>
+            </h2>
         </div>
     );
 };
