ccbf6b3c5d3364de79dba15e817e55b3d6d96932
snaag <snaag.dev@gmail.com>
2024-07-15 00:09:27 +0900

    modify margin Profile Page

--- src/pages/home/Profile.tsx
+++ src/pages/home/Profile.tsx
@@ -8,9 +8,9 @@ const Profile = () => {
             <div className="text-lg flex flex-col items-center text-center md:flex-row md:text-left md:justify-center">
                 <div className="profile__image mb-3 md:mb-0 md:mr-10"/>
                 <div className="flex flex-col">
-                    <span className="mb-0 font-bold md:mb-3">{data.profile.name.ko} ({data.profile.name.nickname})</span>
-                    <span>{data.profile.introduction}</span>
-                    <span>{data.profile.work}</span>
+                    <span className="mb-3 font-bold text-xl md:mb-3">{data.profile.name.ko} ({data.profile.name.nickname})</span>
+                    <span className="mb-2">{data.profile.introduction}</span>
+                    <span className="mb-2">{data.profile.work}</span>
                     <a target="_blank" href={data.profile.links.youtube.url}
                        rel="noreferrer">
                         <span className="profile__youtube">YT</span>{" "}
